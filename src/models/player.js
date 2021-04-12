863f97bec3014bfafcee89b1e4f35720aae6a5fd
cmendoza <clrwxb@gmail.com>
2021-04-11 17:34:51 -0700

    added heroku endpoint to model file

--- src/models/player.js
+++ src/models/player.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
-const url = process.env.REACT_APP_API || "http://localhost:4000/api/v1"
-// const url = process.env.REACT_APP_API || "https://react-sideline-api.herokuapp.com/api/v1"
+// const url = process.env.REACT_APP_API || "http://localhost:4000/api/v1"
+const url = process.env.REACT_APP_API || "https://react-sideline-api.herokuapp.com/api/v1"
 
 
 class PlayerModel {
